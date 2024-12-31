f41a372bcd2e03cdf8e23249f23e120ee8b78165
Mohamed Doumbouya <mohamed.doumbouya@inetum.com>
2024-12-31 22:52:11 +0100

    clean

--- Contact/Contact.App.Core/Contact/UseCase/AddContact/Request/AddContactRequest.cs
+++ Contact/Contact.App.Core/Contact/UseCase/AddContact/Request/AddContactRequest.cs
@@ -1,7 +1,4 @@
-﻿using Domain.Entities;
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace Application.UseCase.AddContact.Request
 {
