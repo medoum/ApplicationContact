becb9118e04d759e475fa3856d17bd12c4f5bb01
Mohamed <mohamedoum2430@gmail.com>
2025-03-16 00:04:31 +0000

    Fix retours

---
+++ Contact/Contact.App.Core/ContactApp/UseCase/ReplaceContact/ReplaceContactUseCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.UseCase.AddContact.Request;
+using Contact.App.Core.ContactApp.Entity;
+using Contact.App.Core.ContactApp.UseCase.AddContact.Request;
+
+namespace Contact.App.Core.ContactApp.UseCase.ReplaceContact
+{
+    public class ReplaceContactUseCase 
+    {
+        private readonly IContactRepository _contactRepository;
+        public ReplaceContactUseCase(IContactRepository contactRepository)
+        {
+            _contactRepository = contactRepository;
+        }
+        public async Task RemplacerContact(ReplaceContactRequest request)
+        {
+
+            var existingContact = await _contactRepository.GetSingleContactAsync(request.contactId);
+            if (existingContact == null)
+                throw new InvalidOperationException($"Contact avec ID {request.contactId} non trouvé");
+
+            existingContact.SetFirstName(request.FirstName);
+            existingContact.SetLastName(request.LastName);
+            existingContact.SetPhoneNumber(request.PhoneNumber);
+            await _contactRepository.UpdateContactAsync(existingContact);
+        }
+    }
+}
