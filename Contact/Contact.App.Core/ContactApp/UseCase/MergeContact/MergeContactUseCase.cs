085b10260e4c8c3e72ff2d97aa56cd77e1fe9fd0
Mohamed Doumbouya <mohamed.doumbouya@inetumsoftware.com>
2025-05-29 09:55:18 +0100

    update mergecontact

--- Contact/Contact.App.Core/ContactApp/UseCase/MergeContact/MergeContactUseCase.cs
+++ Contact/Contact.App.Core/ContactApp/UseCase/MergeContact/MergeContactUseCase.cs
@@ -1,25 +1,45 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Contact.App.Core.ContactApp.Entity;
 using Contact.App.Core.ContactApp.UseCase.AddContact.Request;
 using ContactApp.App.Core.Shared.Exceptions;
 
 namespace Contact.App.Core.ContactApp.UseCase.MergeContact
 {
-    public class MergeContactUseCase 
+    public class MergeContactUseCase
     {
         private readonly IContactRepository _contactRepository;
+
         public MergeContactUseCase(IContactRepository contactRepository)
         {
             _contactRepository = contactRepository;
         }
+
         public async Task<Guid> Execute(MergeContactRequest request)
         {
-
+            // recuperation par id
             var existingContact = await _contactRepository.GetSingleContactAsync(request.PhoneNumber, request.Email);
+           
+            // methode is valid par exemple
+            // plus expressif
             if (existingContact == null)
                 throw new InvalidOperationException(InvalidError.ContactAlreadyExists);
 
-            existingContact.SetAdditionalPhoneNumber(request.PhoneNumber);
+            // Mise à jour des informations du contact si nécessaire
+            //Orchestrer dans le model
+            if (existingContact.GetFirstName() != request.FirstName || existingContact.GetLastName() != request.LastName)
+            {
+                existingContact.SetFirstName(request.FirstName);
+                existingContact.SetLastName(request.LastName);
+            }
+
+            // Ajout du numéro de téléphone si différent
+            //request 
+            if (!string.IsNullOrEmpty(request.PhoneNumber) && existingContact.GetPhoneNumber() != request.PhoneNumber)
+            {
+                existingContact.SetAdditionalPhoneNumber(request.PhoneNumber);
+            }
+
             await _contactRepository.UpdateContactAsync(existingContact);
             return existingContact.GetId();
         }
